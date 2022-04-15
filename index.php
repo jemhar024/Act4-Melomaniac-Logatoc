e0bb2a271b15916787a086dd2ee11823311e5cf6
Jemhar Jhon L. Logatoc <jemharlandoylogatoc@gmail.com>
2022-04-15 16:41:56 +0800

    Song1

--- index.php
+++ index.php
@@ -1,3 +1,45 @@
+<?php
+SESSION_START();
+//Song Titles
+$song_lyrics1 = "Dating Tayo";
+$song_lyrics2 = "Your Song";
+$song_lyrics3 = "Ang Huling El Bimbo";
+$song_lyrics4 = "Beautiful Scars";
+$song_lyrics5 = "One Call Away";
+//current url redirection
+$url_add = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+
+
+//condition to know if the button is clicked
+if(isset($_REQUEST['search_button'])=== true){
+    //song title url
+    if($_REQUEST['song_title'] == $song_lyrics1){
+        header("Location: ".$url_add."?datingtayo");
+    }
+    else if($_REQUEST['song_title'] == $song_lyrics2){
+        header("Location: ".$url_add."?yoursong");
+    }
+    else if($_REQUEST['song_title'] == $song_lyrics3){
+        header("Location: ".$url_add."?anghulingelbimbo");
+    }
+    else if($_REQUEST['song_title'] == $song_lyrics4){
+        header("Location: ".$url_add."?beautifulscars");
+    }
+    else if($_REQUEST['song_title'] == $song_lyrics5){
+        header("Location: ".$url_add."?onecallaway");
+    }
+        //session variable
+        $_SESSION['ses_lyrics1'] = $song_lyrics1;
+        $_SESSION['ses_lyrics2'] = $song_lyrics2;
+        $_SESSION['ses_lyrics3'] = $song_lyrics3;
+        $_SESSION['ses_lyrics4'] = $song_lyrics4;
+        $_SESSION['ses_lyrics5'] = $song_lyrics5;
+          
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -14,14 +56,15 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top"> Add your brand name here </a>
+                <a class="navbar-brand" href="#page-top"> Tune Station </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 1</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
-                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
                           <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
                     </ul>
                 </div>
@@ -30,26 +73,54 @@
         <!-- Header-->
         <header class="bg-dark bg-gradient text-white">
             <div class="container px-4 text-center">
-                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
-                <p class="lead">Add a short description here</p>
+                  <h1 class="fw-bolder">Welcome to Music Station</h1>
+                <p class="lead">"Music is the wine that fills the cup of silence"</p>
                 
             </div>
         </header>
-        <!-- About section-->
+        <!-- About section-->s
         <section id="songs">
             <div class="container px-4 ">
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
-                        <h2 class="text-primary">Song Title here</h2>
-                        <p class="lead">display the Composer here</p>
-                        <p>display the lyrics/chords here</p>
+                        <form method="POST" class="login-form">
+                            <div class="form-group">
+
+
+                                <?php
+                                if(isset($_REQUEST['datingtayo']) === true){
+                                    echo "Redirecting...";
+                                    header ("Refresh: 3; url=song1.php");
+                                }else if(isset($_REQUEST['yoursong'])=== true){
+                                    echo "Redirecting...";
+                                    header ("Refresh: 3; url=song2.php");
+                                }else if(isset($_REQUEST['anghulingelbimbo'])===true){
+                                    echo "Redirecting...";
+                                    header ("Refresh: 3; url=song3.php");
+                                }else if(isset($_REQUEST['beautifulscars'])=== true){
+                                    echo "Redirecting...";
+                                    header ("Refresh: 3; url=song4.php");
+                                }else if(isset($_REQUEST['onecallaway'])=== true){
+                                    echo "Redirecting...";
+                                    header ("Refresh: 3; url=song5.php");
+                                }
+                                    
+                                ?>
+                                <div class="form-group">
+                                    <input type="text" class="form-control rounded-left" placeholder="Song Title here" name="song_title" ></div>
+                                    <button type="submit" class="btn btn-primary rounded submit p-3 px-5" name="search_button"> Search </button>
+                                </div>
+                            </div>
+                        </form>
+
+
                     </div>
                 </div>
             </div>
         </section>
 
         <footer class="py-3 bg-danger">
-            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. add your fullname here</p></div>
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Jemhar Jhon L. Logatoc</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="js/bootstrap.bundle.min.js"></script>
