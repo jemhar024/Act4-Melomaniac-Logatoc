7e361eeccf94b551e8462e403af47b6a7e07212c
Jemhar Jhon L. Logatoc <jemharlandoylogatoc@gmail.com>
2022-04-15 16:53:12 +0800

    Song4

---
+++ song4.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Music Station </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                 <h1 class="fw-bolder">Welcome to Music Station</h1>
+                <p class="lead">"Music is the wine that fills the cup of silence"</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Beautiful Scars</h2>";
+                        echo "<p class='lead '>Maximillian</p>";
+                        echo "<br/>";
+
+                        
+                
+                        echo "<p>Broken all the pieces I've been shaping lately</p>";
+                        echo "<p>Focused on the things that didn't make no sense</p>";
+                        echo "<p> Guess that growing up was never meant to be easy</p>";
+                        echo "<p>Yeah, I got used to doing everything sideways</p>";
+                        echo "<p>Didn't really care about how anyone felt</p>";
+                        echo "<p>Hiding my emotions down in different ashtrays</p>";
+                        echo "<br/>";
+
+                        echo "<p>Oh, but what is lost ain't gone</p>";
+                        echo "<p>No, you can't just let go</p>";
+                        echo "<p>'Cause it's a part of you that will make you strong</p>";
+                        echo "<p>Embrace your flaws</p>";
+                        echo "<br/>";
+
+                        echo "<p>I'm not gonna fight back what I've become</p>";
+                        echo "<p>Yeah, I've got bruises where I came from</p>";
+                        echo "<p>But I wouldn't change if I could restart</p>";
+                        echo "<p>I ain't gonna hide these beautiful scars</p>";
+                        echo "<p>I've been going way too hard on myself</p>";
+                        echo "<p>Guess that it's the reason I've been feeling like hell</p>";
+                        echo "<p>But I wouldn't change if I could restart</p>";
+                        echo "<p>I ain't gonna hide these beautiful scars</p>";
+                        echo "<br/>";
+
+                        echo "<p>I went down a road that only got me nowhere</p>";
+                        echo "<p>I've seen every corner, every inch of this place</p>";
+                        echo "<p>Being all alone it really got me thinking, maybe overthinking</p>";
+                        echo "<br/>";
+
+                        echo "<p>That what is lost ain't gone</p>";
+                        echo "<p>No, you can't just let go</p>";
+                        echo "<p>'Cause it's a part of you that will make you strong</p>";
+                        echo "<p>Embrace your flaws</p>";
+                        echo "<br/>";
+
+                        echo "<p>I'm not gonna fight back what I've become</p>";
+                        echo "<p>Yeah, I've got bruises where I came from</p>";
+                        echo "<p>But I wouldn't change if I could restart</p>";
+                        echo "<p>I ain't gonna hide these beautiful scars</p>";
+                        echo "<p>I've been going way too hard on myself</p>";
+                        echo "<p>Guess that it's the reason I've been feeling like hell</p>";
+                        echo "<p>But I wouldn't change if I could restart</p>";
+                        echo "<p>I ain't gonna hide these beautiful scars</p>";
+                        echo "<br/>";
+
+                        echo "<p>Hide these beautiful scars</p>";
+                        echo "<p>Hide these beautiful scars</p>";
+                        echo "<br/>";
+
+                        echo "<p>I'm not gonna fight back what I've become</p>";
+                        echo "<p>Yeah, I've got bruises where I came from</p>";
+                        echo "<p>But I wouldn't change if I could restart</p>";
+                        echo "<p>I ain't gonna hide these beautiful scars</p>";
+                        echo "<p>I've been going way too hard on myself</p>";
+                        echo "<p>Guess that it's the reason I've been feeling like hell</p>";
+                        echo "<p>But I wouldn't change if I could restart</p>";
+                        echo "<p>I ain't gonna hide these beautiful scars</p>";
+                        echo "<br/>";
+
+                        echo "<p>Hide these</p>";
+                        echo "<p>I ain't gonna hide these</p>";
+                        echo "<p>Hide these</p>";
+                        echo "<p>Hide these beautiful scars</p>";
+                        echo "<p>No</p>";
+                        echo "<br/>";
+
+
+                       
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Jemhar Jhon L. Logatoc</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
